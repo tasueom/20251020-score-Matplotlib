f2d8c6bbd3519f068d11c79a8aac0dccdf51c03a
tasueom <tasueom@naver.com>
2025-10-20 17:31:35 +0900

    feat: 서버 실행 시 index.html로 이동

--- app.py
+++ app.py
@@ -2,6 +2,10 @@ from flask import Flask, render_template as ren, request, redirect, url_for, fla
 
 app = Flask(__name__)
 
+@app.route("/")
+def index():
+    return ren("index.html")
+
 # Flask 서버 실행
 if __name__ == "__main__":
     app.run(debug=True)
