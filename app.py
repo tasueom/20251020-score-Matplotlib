127dd6bc55dde4c8f0d50a3190f3012018c5d524
tasueom <tasueom@naver.com>
2025-10-21 15:03:50 +0900

    chore: 한글 폰트 설정 추가

--- app.py
+++ app.py
@@ -1,6 +1,11 @@
 from flask import Flask, render_template as ren, request, redirect, url_for, flash
 import db
 import numpy as np, pandas as pd, matplotlib.pyplot as plt
+from matplotlib import font_manager, rc
+font_path = "C:/Windows/Fonts/malgun.ttf"
+font_name = font_manager.FontProperties(fname=font_path).get_name()
+rc('font', family = font_name)
+plt.rcParams['axes.unicode_minus'] = False
 
 app = Flask(__name__)
 
